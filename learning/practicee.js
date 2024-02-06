b44f010bba42db3a8a756dc362fccb1e8981342d
Aryan <119997795+Aryan2556@users.noreply.github.com>
2024-02-06 09:39:35 +0000

    updated

--- learning/practicee.js
+++ learning/practicee.js
@@ -4,3 +4,28 @@ let cco=undefined;
 
 console.log(typeof(cco));
 
+console.log(1+"2");
+
+console.log("1"+2);
+
+console.log("1"+"2");
+
+console.log("1" + 2 + 2);
+
+// when performing operations please keep in mind that use more of the paranthesis to improve code readibility eg..
+// 3 + 4 * 5 % 3 ------ not good
+// ( 3 + 4) * (5 % 3) ----- good
+
+console.log(1 + 2 + "2");
+
+console.log(+true);
+
+console.log(+"");
+
+let game=100
+
+game++
+
+console.log(game)
+++game
+console.log(game)
